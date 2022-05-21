af943a3c418cdbddddf8055ce4769bd33812c926
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-21 17:43:44 -0400

    bit of a type constraint issue we need to fix. it's fine for now

--- HelperBitsUT/DepedencyMapTester.cs
+++ HelperBitsUT/DepedencyMapTester.cs
@@ -6,38 +6,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JoshsHelperBits.Collections.Mapper;
 
 namespace HelperBitsUT
 {
     public class DepedencyMapTester : BaseTest,  IDepedencyTesterMethods
     {
-        private class DepedencyMapAdapter<K, V> : IDependencySorter<K, V> where K : struct
+        private class DepedencyMapAdapter<K, V> : IDependencySorter<K, V> where K : struct where V: class
         {
-            public void Add(V item)
+            private IDependencyTree<K, V> _tree;
+            public DepedencyMapAdapter(IDependencyTree<K, V> tree)
             {
-                throw new NotImplementedException();
+                _tree = tree;
             }
+            public void Add(V item) => _tree.Map.Set.Add(item);
 
-            public IEnumerable<V> Data()
-            {
-                throw new NotImplementedException();
-            }
+            public IEnumerable<V> Data() => _tree.GetAllChildItems();
 
-            public IEnumerator<V> GetEnumerator()
-            {
-                throw new NotImplementedException();
-            }
+            public IEnumerator<V> GetEnumerator() => Data().GetEnumerator();
 
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                throw new NotImplementedException();
-            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
+
+        //Looks like we need to fix the Constraint stuff
+        static Func<TProp, KProp> ConstraintHack<KProp,TProp>(Func<TProp, KProp?> selector) where KProp : struct => (x) =>
+            {
+                KProp?r = selector(x);
+                if (r != null)
+                {
+                    return (KProp)r;
+                }
+                else 
+                    return default(KProp);
+            };
 
         IDependencySorter<KProp, TProp> IDepedencyTesterMethods.GetDependencySorter<KProp, TProp>(Func<TProp, KProp?> keySelector1, Func<TProp, KProp?> keySelector2)
-        {
-            throw new NotImplementedException();
-        }
+            => new DepedencyMapAdapter<KProp,TProp>(MapFactory.CreateDependencyTree<KProp, TProp>(ConstraintHack(keySelector1), ConstraintHack( keySelector2)));
 
         private DepedencyTester _dt;
 
