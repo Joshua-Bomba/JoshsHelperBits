91055cd84f64640e53fa9a0f74ba76403b4f9b13
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2023-11-14 15:18:57 -0500

    Some lite testing for now to ensure it's working

---
+++ HelperBitsUT/DuplicatesDictionaryTest.cs
@@ -0,0 +1,36 @@
+﻿using JoshsHelperBits.Collections;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperBitsUT
+{
+    [TestFixture]
+    public class DuplicatesDictionaryTest
+    {
+
+        [Test]
+        public void BasicTest()
+        {
+            //Probably should do more testing
+            DuplicatesDictionary<string, string> dd = new DuplicatesDictionary<string, string>();
+            dd.Add("a", "b");
+            dd.Add("a", "c");
+            dd.Add("b", "d");
+
+            Assert.IsTrue(dd["a"] == "b");
+
+            ILookup<string, string> asLookup = dd;
+            Assert.IsTrue(asLookup["a"].Count() == 2);
+
+
+            dd.Remove("a");
+
+            Assert.IsFalse(dd.ContainsKey("a"));
+
+        }
+    }
+}
